eb9edbefe996ea9ef72f6b39c3fada58c7aac428
RUOXING SUN <ryuichist@hotmail.com>
2016-03-08 16:18:09 -0600

    3月8日下午4:17            增加修正难度选择，UI

--- app/src/main/java/com/mcm/rsun/mentalcalculationmaster/DifficultySelectionActivity.java
+++ app/src/main/java/com/mcm/rsun/mentalcalculationmaster/DifficultySelectionActivity.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class DifficultySelectionActivity extends AppCompatActivity {
+    public static int max_problem_count = 10;
+    public static int sign = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +36,8 @@ public class DifficultySelectionActivity extends AppCompatActivity {
     }
 
     public void select_digit(View v){
+        TextView digit_selection_note = (TextView)findViewById(R.id.digit_selection_note);
+
         switch(v.getId()){
             case R.id.one_digit:
                 GameScreenActivity.number_range = 10;
@@ -45,8 +51,32 @@ public class DifficultySelectionActivity extends AppCompatActivity {
         }
     }
 
-    public void sign_selection(View v){
+    public void select_sign(View v){
+        switch(v.getId()){
+            case R.id.add_only:
+                sign = 1;
+                break;
+            case R.id.subs_only:
+                sign = 2;
+                break;
+            case R.id.add_subs:
+                sign = 3;
+                break;
+        }
+    }
 
+    public void select_problem_count(View v){
+        switch(v.getId()){
+            case R.id.count_10:
+                max_problem_count=10;
+                break;
+            case R.id.count_20:
+                max_problem_count=20;
+                break;
+            case R.id.count_30:
+                max_problem_count=30;
+                break;
+        }
     }
 
 
--- app/src/main/java/com/mcm/rsun/mentalcalculationmaster/GameScreenActivity.java
+++ app/src/main/java/com/mcm/rsun/mentalcalculationmaster/GameScreenActivity.java
@@ -1,11 +1,13 @@
 package com.mcm.rsun.mentalcalculationmaster;
 
-import android.content.Intent;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.GridLayout;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -16,11 +18,12 @@ public class GameScreenActivity extends AppCompatActivity {
     public static int num1;
     public static int num2;
     public static int correct_answer;
-    public static final int MAX_PROBLEM_COUNT = 5;
     public static int current_count = 0;
     public static int correct_count = 0;
     public static int difficulty_level = 0;
-    public static int number_range = -1;
+    public static int number_range = 10;
+    public static final String PLUS = "+";
+    public static final String MINUS = "-";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +38,18 @@ public class GameScreenActivity extends AppCompatActivity {
     public void clicked(View v){
         EditText user_result_view = (EditText)findViewById(R.id.user_result);
         switch (v.getId()){
+            case R.id.change_sign_btn:
+                if(user_result != 0){
+                    user_result *= -1;
+                    user_result_view.setText(Integer.toString(user_result));
+                }
+                break;
+            case R.id.change_sign_btn_2:
+                if(user_result != 0){
+                    user_result *= -1;
+                    user_result_view.setText(Integer.toString(user_result));
+                }
+                break;
             case R.id.button1:
                 user_result = user_result *10 + 1;
                 user_result_view.setText(Integer.toString(user_result));
@@ -82,7 +97,6 @@ public class GameScreenActivity extends AppCompatActivity {
                 break;
             case R.id.button_enter:
                 user_result = Integer.parseInt(user_result_view.getText().toString());
-                System.out.println("user input " + user_result);
                 if(user_result == correct_answer){
                     System.out.println("CORRECT");
                     correct_count++;
@@ -96,20 +110,49 @@ public class GameScreenActivity extends AppCompatActivity {
 
 
     public void generate_question(){
-        current_count++;
-        TextView current_process = (TextView) findViewById(R.id.current_process);
-        TextView correct_rate = (TextView) findViewById(R.id.correct_rate);
-        current_process.setText(current_count + " / " + MAX_PROBLEM_COUNT);
-        correct_rate.setText(correct_count + " / " + MAX_PROBLEM_COUNT);
-        reset_input();
-        Random r = new Random();
-        num1 = r.nextInt(number_range);
-        num2 = r.nextInt(number_range);
-        TextView num1_view = (TextView) findViewById(R.id.num1);
-        TextView num2_view = (TextView) findViewById(R.id.num2);
-        num1_view.setText(Integer.toString(num1));
-        num2_view.setText(Integer.toString(num2));
-        correct_answer = num1+num2;
+        System.out.println("current_count " + current_count + "~~~~~~~~~~~~~~~~~~~~~~~~~~" );
+        if(current_count > DifficultySelectionActivity.max_problem_count){
+            GridLayout keyboard = (GridLayout)findViewById(R.id.keyboard);
+            keyboard.setClickable(false);
+            System.out.println("1111111111111111111111111111");
+        }else{
+            reset_input();
+            current_count++;
+            TextView current_process = (TextView) findViewById(R.id.current_process);
+            TextView correct_rate = (TextView) findViewById(R.id.correct_rate);
+            current_process.setText(current_count + " / " + DifficultySelectionActivity.max_problem_count);
+            correct_rate.setText(correct_count + " / " + DifficultySelectionActivity.max_problem_count);
+
+            Random r = new Random();
+            num1 = r.nextInt(number_range);
+            num2 = r.nextInt(number_range);
+            TextView num1_view = (TextView) findViewById(R.id.num1);
+            TextView num2_view = (TextView) findViewById(R.id.num2);
+            num1_view.setText(Integer.toString(num1));
+            num2_view.setText(Integer.toString(num2));
+
+            TextView sign = (TextView)findViewById(R.id.sign);
+            switch(DifficultySelectionActivity.sign){
+                case 1:
+                    correct_answer = num1+num2;
+                    sign.setText(PLUS);
+                    break;
+                case 2:
+                    correct_answer = num1-num2;
+                    sign.setText(MINUS);
+                    break;
+                case 3:
+                    int s = r.nextInt(2);
+                    if(s == 0){
+                        correct_answer = num1+num2;
+                        sign.setText(PLUS);
+                    }else{
+                        correct_answer = num1-num2;
+                        sign.setText(MINUS);
+                    }
+                    break;
+            }
+        }
     }
 
     public void reset_input(){
@@ -123,13 +166,18 @@ public class GameScreenActivity extends AppCompatActivity {
         correct_count = 0;
     }
 
-    public void exit_clicked(View view){
-        Intent exit_intent = new Intent(this, HomeActivity.class);
-        startActivity(exit_intent);
+    public void exit_clicked(View v) {
+        new AlertDialog.Builder(this)
+                .setMessage("Are you sure you want to exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
 
-    public void onDestory(){
-        super.onDestroy();
-        this.finish();
-    }
+
 }
